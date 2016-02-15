1370fdec771fa60d335b39916cdee8a73ea6a62e
Andrea Celentano <andrea.celentano@ge.infn.it>
2016-02-15 19:03:49 +0100

    Working on calorimeter

--- src/libraries/Calorimeter/Calorimeterfa250Converter.cc
+++ src/libraries/Calorimeter/Calorimeterfa250Converter.cc
@@ -23,32 +23,38 @@ CalorimeterSiPMHit* Calorimeterfa250Converter::convertHit(const fa250Hit *hit,co
 
 jerror_t Calorimeterfa250Converter::convertMode1Hit(CalorimeterSiPMHit* output,const fa250Mode1CalibHit *input) const{
 	int size=input->samples.size();
-	double Q=0;
-	double ped=0;
 
+	output->Q=0;
+	output->T=0;
+	output->A=0;
+	/*This part is to read pedestal from dB*/
 	vector<double> pedV;
 	pedestal->getCalib(output->m_channel.calorimeter,pedV);
-	if (pedV.size()!=1){
-		jerr<<"Calorimeterfa250Converter::convertMode1Hit error on pedestal db entries. Got: "<<pedV.size()<<std::endl;
-		ped=0;
+	if (pedV.size()==1){
+		output->ped=pedV.at(0);
 	}
 	else{
-		ped=pedV.at(0);
+		jerr<<"Calorimeterfa250Converter::convertMode1Hit error on pedestal db entries. Got: "<<pedV.size()<<std::endl;
+		output->ped=0;  //A.C. for now ok, in the future need to revert-back to the non-db case!*/
 	}
 
+	/*Charge-amplitude computation*/
+	output->A=-9999; //A.C will this always work?
 	for (int ii=0;ii<size;ii++){
-		Q+=(input->samples.at(ii)-ped);
+		output->Q+=(input->samples.at(ii)-output->ped);
+		if ((input->samples.at(ii) - output->ped) > output->A){
+			output->A = (input->samples.at(ii) - output->ped);
+			output->T = ii*4.; //a.c. very dummy for now! also, 4 ns/sample is hard-coded.
+		}
 	}
+
 	output->miniped=0;
-	for (int ii=0;ii<10;ii++){
+	for (int ii=0;ii<NPED;ii++){
 			output->miniped+=(input->samples.at(ii));
 		}
-	output->miniped/=10;
+	output->miniped/=NPED;
 
 
-	output->ped=Q/size;
-	output->Q=Q;
-	output->T=0;
 
 	return NOERROR;
 }
