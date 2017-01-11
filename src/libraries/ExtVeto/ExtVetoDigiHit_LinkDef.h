1f971c2da1954534a5b9f6473dd3c6d0b62c3013
Andrea Celentano <andrea.celentano@ge.infn.it>
2017-01-11 13:14:15 +0100

    Missing files

---
+++ src/libraries/ExtVeto/ExtVetoDigiHit_LinkDef.h
@@ -0,0 +1,14 @@
+#include "ExtVetoDigiHit.h"
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+#pragma link C++ class ExtVetoDigiHit+;
+#pragma link C++ struct ExtVetoDigiHit::ExtVetoPMTDigiHit+;
+#pragma link C++ class vector<ExtVetoDigiHit::ExtVetoPMTDigiHit>+;
+
+#pragma link C++ class vector<ExtVetoDigiHit>+;
+#pragma link C++ class vector<ExtVetoDigiHit*>+;
+#endif
---
+++ src/libraries/MC/MCType.h
@@ -0,0 +1,21 @@
+#ifndef MCTYPE_H_
+#define MCTYPE_H_
+
+
+#include <JANA/JObject.h>
+#include <JANA/JFactory.h>
+
+class MCType {
+
+
+public:
+	enum TYPE{
+			CATANIA_V1=1,
+			CATANIA_V2=2,
+			FULL_V1=10,
+	};
+
+
+};
+
+#endif /* VETOMCHIT_H_ */
