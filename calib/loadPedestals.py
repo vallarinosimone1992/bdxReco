67664dfa7265a9d03c33821811e65f3ba64613f1
Andrea Celentano <andrea.celentano@ge.infn.it>
2017-11-07 11:27:45 +0100

    loadPedestals works also for JLabFlux0

--- calib/loadPedestals.py
+++ calib/loadPedestals.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-                                                                                                                                                                                                                             
 
 import string,math,os,sys,subprocess
-import argparse
+import argparse,glob
 
 #check current working dir
 if ("BDXRECO_ROOT" in os.environ)==0:
@@ -30,14 +30,16 @@ if (cur_fold != bdx_fold):
 
 
 parser = argparse.ArgumentParser(description='Load data in CCDB for given variation / runs')
-parser.add_argument('--variation',type=str, default="default", help='If set, set the variation to use. The variation should correspond to a folder with the txt files to upload')
+parser.add_argument('--variation',type=str, default="default", help='If set, set the variation to use.')
 parser.add_argument('--connection',type=str,required=True,help="Connection string to use")
 parser.add_argument('--firstRun',type=int,default=1000,help="minimum run to consider in the folder")
+parser.add_argument('--lastRun',type=int,default=1001,help="maximum run to consider in the folder")
 args = parser.parse_args()
 
 variation = args.variation
 connection_string = args.connection
 firstRun = args.firstRun
+lastRun = args.lastRun
 #check if DAQ_pedestals folder exists
 if (os.path.isdir("DAQ_pedestals")==False):
     print "Error, folder DAQ_pedestals does not exists"
@@ -47,26 +49,38 @@ commandBase = "ccdb -c "+connection_string+" add -v "+variation
 
 print "Loading pedestals for variation ",variation," and run_min: ",firstRun
 
-#now need to loop over ALL the files there
-for fn in os.listdir("DAQ_pedestals"):
-    if os.path.isfile("DAQ_pedestals/"+fn):
-        #fn is in the form: runxxxxx.ped
+
+#Depending on variation:
+
+#In case of default, do nothing
+if (variation=="default"):
+    outF=open("DAQ_pedestals/tables/default.dat","w")
+    for crate in range(0,20):
+        for slot in range(0,20):
+            for channel in range(0,16):
+                outF.write("0 "+str(slot)+" "+str(channel)+" 0 0 \n")
+                outF.close()
+    #at this point, we have the file for run 
+    #specify in the command
+    command = commandBase+" -r "+str(firstRun)+"-"+str(lastRun)
+    command += " /DAQ/pedestals DAQ_pedestals/tables/default.dat"
+    print command
+    subprocess.Popen(command,shell=True)
+
+
+#In case of other variations, need to open file with slot1 only
+else:
+    for fn in glob.glob("DAQ_pedestals/run*.ped"): #this may be variation-dependent
+        print fn
+        #fn is in the form: absPath/runxxxxx.ped
+        fn=os.path.basename(fn)
         run=fn[3:].split('.')[0]
         if (int(run)<firstRun):
             continue
-        print("Run number: "+run)
-        outF=open("DAQ_pedestals/tables/run_"+run+".dat","w")
-        #write the first crate up to slot #3
-        for slot in range(0,4):
-            for channel in range(0,16):
-                outF.write("0 "+str(slot)+" "+str(channel)+" 0 0 \n")
-        
-        #in case of default variation, do not load
-        if (variation=="default"):
-            for slot in range(4,10):
-                for channel in range(0,16):
-                       outF.write("0 "+str(slot)+" "+str(channel)+" "+"0 0 \n")
-        else: #load
+        if (int(run)>lastRun):
+            continue
+        outF=open("DAQ_pedestals/tables/run_"+run+".dat","w") #this may be variation-dependent
+        if (variation=="JLabFlux0"): #Read slot 1, then write all the others
             with open("DAQ_pedestals/"+fn) as inF:
                 for line in inF:
                     data=line.split()
@@ -76,19 +90,33 @@ for fn in os.listdir("DAQ_pedestals"):
                     RMS=data[3]
                     outF.write("0 "+str(slot)+" "+str(channel)+" "+str(ped)+" "+str(RMS)+"\n")
             inF.close()
-        #done up to slot 9. need to proceed up to 19
-        for slot in range(10,20):
-            for channel in range(0,16):
-                outF.write("0 "+str(slot)+" "+str(channel)+" 0 0\n")        
+            for slot in range(1,20):
+                for channel in range(0,16):
+                    outF.write("0 "+str(slot)+" "+str(channel)+" 0 0 \n")
+        else:  #Write slots 1-3, then read 4-10, then write 11-19
+            for slot in range(0,4):
+                for channel in range(0,16):
+                    outF.write("0 "+str(slot)+" "+str(channel)+" 0 0 \n")
+            with open("DAQ_pedestals/"+fn) as inF:
+                for line in inF:
+                    data=line.split()
+                    slot=data[0]
+                    channel=data[1]
+                    ped=data[2]
+                    RMS=data[3]
+                    outF.write("0 "+str(slot)+" "+str(channel)+" "+str(ped)+" "+str(RMS)+"\n")
+            inF.close()
+            for slot in range(10,20):
+                for channel in range(0,16):
+                    outF.write("0 "+str(slot)+" "+str(channel)+" 0 0 \n")
+        #All variations write other crates
         for crate in range(1,20):
-                for slot in range(0,20):
-                    for channel in range(0,16):
-                        outF.write(str(crate)+" "+str(slot)+" "+str(channel)+" 0 0\n")        
-        outF.close()
+            for slot in range(0,20):
+                for channel in range(0,16):
+                    outF.write(str(crate)+" "+str(slot)+" "+str(channel)+" 0 0\n")        
         #at this point, we have the file for run 
         #specify in the command
         command = commandBase+" -r "+str(run)+"-"+str(run)
         command += " /DAQ/pedestals DAQ_pedestals/tables/run_"+run+".dat"
         print command
         subprocess.Popen(command,shell=True)
-
